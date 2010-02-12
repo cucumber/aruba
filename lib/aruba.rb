703e606d04fe177c980b9b06b76c2941505f672c
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2010-02-12 16:12:32 -0400

    exit statuses feature

--- lib/aruba.rb
+++ lib/aruba.rb
@@ -44,3 +44,7 @@ end
 Then /^I should see exactly:$/ do |exact_output|
   @last_stdout.should == exact_output
 end
+
+Then /^the exit status should be (\d+)$/ do |exit_status|
+  @last_exit_status.should == exit_status.to_i
+end
