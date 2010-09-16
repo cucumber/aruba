336bfe1e44e6c8f9e9c447261ce438a19116461a
aslakhellesoy <aslak.hellesoy@gmail.com>
2010-09-16 16:34:04 +0200

    indent

--- lib/aruba/api.rb
+++ lib/aruba/api.rb
@@ -2,161 +2,161 @@ require 'tempfile'
 require 'rbconfig'
 
 module Aruba
-module Api
-  def in_current_dir(&block)
-    _mkdir(current_dir)
-    Dir.chdir(current_dir, &block)
-  end
-
-  def current_dir
-    File.join(*dirs)
-  end
-
-  def cd(dir)
-    dirs << dir
-    raise "#{current_dir} is not a directory." unless File.directory?(current_dir)
-  end
-
-  def dirs
-    @dirs ||= ['tmp/aruba']
-  end
-
-  def create_file(file_name, file_content)
-    in_current_dir do
-      _mkdir(File.dirname(file_name))
-      File.open(file_name, 'w') { |f| f << file_content }
+  module Api
+    def in_current_dir(&block)
+      _mkdir(current_dir)
+      Dir.chdir(current_dir, &block)
     end
-  end
 
-  def append_to_file(file_name, file_content)
-    in_current_dir do
-      File.open(file_name, 'a') { |f| f << file_content }
+    def current_dir
+      File.join(*dirs)
     end
-  end
 
-  def create_dir(dir_name)
-    in_current_dir do
-      _mkdir(dir_name)
+    def cd(dir)
+      dirs << dir
+      raise "#{current_dir} is not a directory." unless File.directory?(current_dir)
     end
-  end
 
-  def check_file_presence(paths, expect_presence)
-    in_current_dir do
-      paths.each do |path|
-        if expect_presence
-          File.should be_file(path)
-        else
-          File.should_not be_file(path)
+    def dirs
+      @dirs ||= ['tmp/aruba']
+    end
+
+    def create_file(file_name, file_content)
+      in_current_dir do
+        _mkdir(File.dirname(file_name))
+        File.open(file_name, 'w') { |f| f << file_content }
+      end
+    end
+
+    def append_to_file(file_name, file_content)
+      in_current_dir do
+        File.open(file_name, 'a') { |f| f << file_content }
+      end
+    end
+
+    def create_dir(dir_name)
+      in_current_dir do
+        _mkdir(dir_name)
+      end
+    end
+
+    def check_file_presence(paths, expect_presence)
+      in_current_dir do
+        paths.each do |path|
+          if expect_presence
+            File.should be_file(path)
+          else
+            File.should_not be_file(path)
+          end
         end
       end
     end
-  end
 
-  def check_file_content(file, partial_content, expect_match)
-    regexp = compile_and_escape(partial_content)
-    in_current_dir do
-      content = IO.read(file)
-      if expect_match
-        content.should =~ regexp
-      else
-        content.should_not =~ regexp
+    def check_file_content(file, partial_content, expect_match)
+      regexp = compile_and_escape(partial_content)
+      in_current_dir do
+        content = IO.read(file)
+        if expect_match
+          content.should =~ regexp
+        else
+          content.should_not =~ regexp
+        end
       end
     end
-  end
   
-  def check_directory_presence(paths, expect_presence)
-    in_current_dir do
-      paths.each do |path|
-        if expect_presence
-          File.should be_directory(path)
-        else
-          File.should_not be_directory(path)
+    def check_directory_presence(paths, expect_presence)
+      in_current_dir do
+        paths.each do |path|
+          if expect_presence
+            File.should be_directory(path)
+          else
+            File.should_not be_directory(path)
+          end
         end
       end
     end
-  end
 
-  def _mkdir(dir_name)
-    FileUtils.mkdir_p(dir_name) unless File.directory?(dir_name)
-  end
-
-  def unescape(string)
-    eval(%{"#{string}"})
-  end
-
-  def compile_and_escape(string)
-    Regexp.compile(Regexp.escape(string))
-  end
-
-  def combined_output
-    @last_stdout + (@last_stderr == '' ? '' : "\n#{'-'*70}\n#{@last_stderr}")
-  end
-
-  def install_gems(gemfile)
-    create_file("Gemfile", gemfile)
-    if ENV['GOTGEMS'].nil?
-      run("gem install bundler")
-      run("bundle --no-color install")
+    def _mkdir(dir_name)
+      FileUtils.mkdir_p(dir_name) unless File.directory?(dir_name)
     end
-  end
 
-  def run(cmd, fail_on_error=true)
-    cmd = detect_ruby_script(cmd)
-    cmd = detect_ruby(cmd)
+    def unescape(string)
+      eval(%{"#{string}"})
+    end
 
-    announce_or_puts("$ #{cmd}") if @announce_cmd
+    def compile_and_escape(string)
+      Regexp.compile(Regexp.escape(string))
+    end
 
-    stderr_file = Tempfile.new('cucumber')
-    stderr_file.close
-    in_current_dir do
-      mode = RUBY_VERSION =~ /^1\.9/ ? {:external_encoding=>"UTF-8"} : 'r'
-      IO.popen("#{cmd} 2> #{stderr_file.path}", mode) do |io|
-        @last_stdout = io.read
+    def combined_output
+      @last_stdout + (@last_stderr == '' ? '' : "\n#{'-'*70}\n#{@last_stderr}")
+    end
 
-        announce_or_puts(@last_stdout) if @announce_stdout
+    def install_gems(gemfile)
+      create_file("Gemfile", gemfile)
+      if ENV['GOTGEMS'].nil?
+        run("gem install bundler")
+        run("bundle --no-color install")
+      end
+    end
+
+    def run(cmd, fail_on_error=true)
+      cmd = detect_ruby_script(cmd)
+      cmd = detect_ruby(cmd)
+
+      announce_or_puts("$ #{cmd}") if @announce_cmd
+
+      stderr_file = Tempfile.new('cucumber')
+      stderr_file.close
+      in_current_dir do
+        mode = RUBY_VERSION =~ /^1\.9/ ? {:external_encoding=>"UTF-8"} : 'r'
+        IO.popen("#{cmd} 2> #{stderr_file.path}", mode) do |io|
+          @last_stdout = io.read
+
+          announce_or_puts(@last_stdout) if @announce_stdout
+        end
+
+        @last_exit_status = $?.exitstatus
+      end
+      @last_stderr = IO.read(stderr_file.path)
+
+      announce_or_puts(@last_stderr) if @announce_stderr
+
+      if(@last_exit_status != 0 && fail_on_error)
+        fail("Exit status was #{@last_exit_status}. Output:\n#{combined_output}")
       end
 
-      @last_exit_status = $?.exitstatus
-    end
-    @last_stderr = IO.read(stderr_file.path)
-
-    announce_or_puts(@last_stderr) if @announce_stderr
-
-    if(@last_exit_status != 0 && fail_on_error)
-      fail("Exit status was #{@last_exit_status}. Output:\n#{combined_output}")
+      @last_stderr
     end
 
-    @last_stderr
-  end
+    def announce_or_puts(msg)
+      if(@puts)
+        puts(msg)
+      else
+        announce(msg)
+      end
+    end
 
-  def announce_or_puts(msg)
-    if(@puts)
-      puts(msg)
-    else
-      announce(msg)
+    def detect_ruby(cmd)
+      if cmd =~ /^ruby\s/
+        cmd.gsub(/^ruby\s/, "#{current_ruby} ")
+      else
+        cmd
+      end
+    end
+
+    COMMON_RUBY_SCRIPTS = /^(?:bundle|cucumber|gem|jeweler|rails|rake|rspec|spec)\s/
+
+    def detect_ruby_script(cmd)
+      if cmd =~ COMMON_RUBY_SCRIPTS
+        "ruby -S #{cmd}"
+      else
+        cmd
+      end
+    end
+
+    def current_ruby
+      File.join(Config::CONFIG['bindir'], Config::CONFIG['ruby_install_name'])
     end
   end
-
-  def detect_ruby(cmd)
-    if cmd =~ /^ruby\s/
-      cmd.gsub(/^ruby\s/, "#{current_ruby} ")
-    else
-      cmd
-    end
-  end
-
-  COMMON_RUBY_SCRIPTS = /^(?:bundle|cucumber|gem|jeweler|rails|rake|rspec|spec)\s/
-
-  def detect_ruby_script(cmd)
-    if cmd =~ COMMON_RUBY_SCRIPTS
-      "ruby -S #{cmd}"
-    else
-      cmd
-    end
-  end
-
-  def current_ruby
-    File.join(Config::CONFIG['bindir'], Config::CONFIG['ruby_install_name'])
-  end
-end
 end
