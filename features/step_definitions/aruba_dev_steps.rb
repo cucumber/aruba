09051e47487ae91e550900d7fd98ed066c7f79d5
aslakhellesoy <aslak.hellesoy@gmail.com>
2010-09-29 00:32:54 +0200

    Remove unused stepdefs. Thanks -f usage.

--- features/step_definitions/aruba_dev_steps.rb
+++ features/step_definitions/aruba_dev_steps.rb
@@ -1,22 +1,9 @@
-Given /^I have a local file named "([^"]*)" with:$/ do |filename, content|
-  File.open(filename, 'w') {|io| io.write(content)}
-end
-
 When /^I do aruba (.*)$/ do |aruba_step|
   begin
     When(aruba_step)
   rescue => e
     @aruba_exception = e
   end
-end
-
-Then /^the output should contain the JRuby version$/ do
-  pending "This must be manually run in JRuby" unless defined?(JRUBY_VERSION)
-  Then %{the output should contain "#{JRUBY_VERSION}"}
-end
-
-Then /^the output should contain the current Ruby version$/ do
-  Then %{the output should contain "#{RUBY_VERSION}"}
 end
 
 Then /^aruba should fail with "([^"]*)"$/ do |error_message|
