e28ea09f350aa9faa38ea07b1965e340aabfe03e
aslakhellesoy <aslak.hellesoy@gmail.com>
2010-12-26 16:57:29 -0600

    Nobody uses that old stuff

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,15 +1,4 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__) + '/../../lib')
 require 'aruba/cucumber'
 require 'fileutils'
-
-begin
-  # rspec-2
-  require 'rspec/expectations'
-rescue LoadError
-  # rspec-1
-  require 'spec/expectations'
-end
-
-Before do
-  FileUtils.rm(Dir['config/*.yml'])
-end
+require 'rspec/expectations'
