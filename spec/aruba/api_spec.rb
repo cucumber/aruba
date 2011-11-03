b9c7e9cfb72ec14d9ce58a171944e439ac83d3ff
Robert Wahler <robert@gearheadforhire.com>
2011-11-03 13:24:19 -0400

    add failing spec for @announce_stdout tag

--- spec/aruba/api_spec.rb
+++ spec/aruba/api_spec.rb
@@ -2,11 +2,42 @@ require 'spec_helper'
 
 describe Aruba::Api  do
 
-  describe 'current_dir' do
+  before(:each) do
+    klass = Class.new {
+      include Aruba::Api
 
+      def announce=(value)
+        @announce_stdout = value
+      end
+    }
+    @aruba = klass.new
+  end
+
+  describe 'current_dir' do
     it "should return the current dir as 'tmp/aruba'" do
-      current_dir.should match(/^tmp\/aruba$/)
+      @aruba.current_dir.should match(/^tmp\/aruba$/)
     end
   end
 
+  describe 'run' do
+
+    context 'with the @announce_stdout tag' do
+      it "should announce to stdout exactly once" do
+        @aruba.should_receive(:announce_or_puts).once
+        @aruba.announce = true
+        @aruba.run("ruby -e 'puts \"hello world\"'")
+        @aruba.all_output.should match(/hello world/)
+      end
+    end
+
+    context 'without @announce_stdout tag' do
+      it "should not announce to stdout" do
+        @aruba.should_not_receive(:announce_or_puts)
+        @aruba.announce = false
+        @aruba.run("ruby -e 'puts \"hello world\"'")
+        @aruba.all_output.should match(/hello world/)
+      end
+    end
+
+  end
 end
