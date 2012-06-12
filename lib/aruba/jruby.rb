58c468a2aaadb1bbbc460ccd88e49900e8d31a2a
michael <michael@iconoclast.net>
2012-06-12 11:56:54 -0700

    Fix for Jruby over-optimization causing failure on jvm mismatch
    Only Solaris supports switching between 32-bit and 64-bit mode in
    the same binary.  Every other platform has separate java binaries
    and the -d32 (or -d64) switch will either do nothing or cause the
    program to exit with a failure code.

--- lib/aruba/jruby.rb
+++ lib/aruba/jruby.rb
@@ -1,7 +1,9 @@
+require 'rbconfig'
 Aruba.configure do |config|
   config.before_cmd do
     # ideas taken from: http://blog.headius.com/2010/03/jruby-startup-time-tips.html
     set_env('JRUBY_OPTS', '-X-C') # disable JIT since these processes are so short lived
-    set_env('JAVA_OPTS', '-d32')  # force jRuby to use client JVM for faster startup times
+    # force jRuby to use client JVM for faster startup times. Not all systems support this.
+    set_env('JAVA_OPTS', '-d32')  if Config::CONFIG['host_os'] =~ /solaris|sunos/i
   end
 end
