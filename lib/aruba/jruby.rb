d52f02d79a6d2f7e81c0ed3731d3cc583b6aa9c7
michael <michael@iconoclast.net>
2012-06-21 12:02:42 -0700

    Use RbConfig instead of Config

--- lib/aruba/jruby.rb
+++ lib/aruba/jruby.rb
@@ -4,6 +4,6 @@ Aruba.configure do |config|
     # ideas taken from: http://blog.headius.com/2010/03/jruby-startup-time-tips.html
     set_env('JRUBY_OPTS', '-X-C') # disable JIT since these processes are so short lived
     # force jRuby to use client JVM for faster startup times. Not all systems support this.
-    set_env('JAVA_OPTS', '-d32')  if Config::CONFIG['host_os'] =~ /solaris|sunos/i
+    set_env('JAVA_OPTS', '-d32')  if RbConfig::CONFIG['host_os'] =~ /solaris|sunos/i
   end
 end
