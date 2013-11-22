8759c591bb2c806c1236837df062d90cd5bae8f7
Dennis Günnewig <dg1@vrnetze.de>
2013-11-22 14:31:11 +0100

    Implemented file matchers

---
+++ lib/aruba/matchers/file_regex_matcher.rb
@@ -0,0 +1,27 @@
+module Aruba
+  module Matchers
+    class FileRegexMatcher
+      include RSpec::Expectations
+      include RSpec::Matchers
+
+      def initialize( expect_presence )
+        @expect_presence = expect_presence
+      end
+
+      def check(regexes)
+        found_files = Dir.glob('**/*')
+
+        Array( regexes ).each do |r|
+          result = found_files.grep( Regexp.new( r ) ).empty?
+
+          if @expect_presence
+            expect( result ).not_to be_true
+          else
+            expect( result ).to be_true
+          end
+        end
+      end
+
+    end
+  end
+end
---
+++ lib/aruba/matchers/file_simple_matcher.rb
@@ -0,0 +1,22 @@
+module Aruba
+  module Matchers
+    class FileSimpleMatcher
+      include RSpec::Expectations
+      include RSpec::Matchers
+
+      def initialize( expect_presence )
+        @expect_presence = expect_presence
+      end
+
+      def check(paths)
+        Array( paths ).each do |p|
+          if @expect_presence
+            expect( File.exists? p ).to be_true
+          else
+            expect( File.exists? p ).not_to be_true
+          end
+        end
+      end
+    end
+  end
+end
