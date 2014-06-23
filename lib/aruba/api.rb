481f24ae0d70d0930918bb5967988f055ec66187
Dennis Günnewig <dg1@vrnetze.de>
2014-06-23 09:36:09 +0200

    Add explanation to dirs

--- lib/aruba/api.rb
+++ lib/aruba/api.rb
@@ -42,6 +42,11 @@ module Aruba
       raise "#{current_dir} is not a directory." unless File.directory?(current_dir)
     end
 
+    # The path to the directory which should contain all your test data
+    # You might want to overwrite this method to place your data else where.
+    #
+    # @return [Array]
+    #   The directory path: Each subdirectory is a member of an array
     def dirs
       @dirs ||= ['tmp', 'aruba']
     end
