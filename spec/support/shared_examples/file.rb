1b1e3400dd1a2d86ab2ceaea931234facefd3b59
Dennis Günnewig <dg1@ratiodata.de>
2015-02-09 15:10:12 +0100

    Make sure it exists and is a file

--- spec/support/shared_examples/file.rb
+++ spec/support/shared_examples/file.rb
@@ -1,5 +1,5 @@
 shared_examples 'an existing file' do
-  it { Array(file_path).each { |p| expect(File).to be_exist(p) } }
+  it { Array(file_path).each { |p| expect(File).to be_file(p) } }
 end
 
 shared_examples 'a non-existing file' do
