7b8911dea5fc52a219e049634862ae617367e272
Dennis Günnewig <dg1@ratiodata.de>
2015-02-09 15:28:09 +0100

    Fixed type in error messages

--- lib/aruba/matchers/mode.rb
+++ lib/aruba/matchers/mode.rb
@@ -17,13 +17,13 @@ RSpec::Matchers.define :have_permissions do |expected|
     file_name = actual
     actual_permissions = sprintf( "%o", File::Stat.new(absolute_path(file_name)).mode )[-4,4].gsub(/^0*/, '')
 
-    format("expected that file \"%s\" would have permissions \"%s\", but has \"\%s\".", file_name, expected_permissions, actual_permissions)
+    format("expected that file \"%s\" would have permissions \"%s\", but has \"%s\".", file_name, expected_permissions, actual_permissions)
   end
 
   failure_message_when_negated do |actual|
     file_name = actual
     actual_permissions = sprintf( "%o", File::Stat.new(absolute_path(file_name)).mode )[-4,4].gsub(/^0*/, '')
 
-    format("expected that file \"%s\" would not have permissions \"%s\", but has \"\%s\".", file_name, expected_permissions, actual_permissions)
+    format("expected that file \"%s\" would not have permissions \"%s\", but has \"%s\".", file_name, expected_permissions, actual_permissions)
   end
 end
