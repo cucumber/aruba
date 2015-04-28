a006632d61637c232a65dcf2d89ac7a8b8fb701e
Dennis Günnewig <dg1@ratiodata.de>
2015-04-28 14:02:48 +0200

    Added test for file content matcher

--- spec/aruba/matchers/file_spec.rb
+++ spec/aruba/matchers/file_spec.rb
@@ -20,4 +20,32 @@ RSpec.describe 'File Matchers' do
       it { expect(@file_name).not_to be_existing_file }
     end
   end
+
+  describe 'to_have_content' do
+    context 'when file exists' do
+      before :each do
+        File.write(@file_path, 'aba')
+      end
+
+      context 'and file content is exactly equal string ' do
+        it { expect(@file_name).to have_content('aba') }
+      end
+
+      context 'and file content contains string' do
+        it { expect(@file_name).to have_content(/b/) }
+      end
+
+      context 'and file content is not exactly equal string' do
+        it { expect(@file_name).not_to have_content('c') }
+      end
+
+      context 'and file content not contains string' do
+        it { expect(@file_name).not_to have_content(/c/) }
+      end
+    end
+
+    context 'when file does not exist' do
+      it { expect(@file_name).not_to have_content('a') }
+    end
+  end
 end
