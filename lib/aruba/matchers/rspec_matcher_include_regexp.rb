4822f09bde7be2c2a3b0a767cc819e075a455845
Dennis Günnewig <dg1@ratiodata.de>
2015-04-30 12:55:51 +0200

    Deprecate include_regexp-matcher it was introduced as util-matcher and is not needed anymore.

--- lib/aruba/matchers/rspec_matcher_include_regexp.rb
+++ lib/aruba/matchers/rspec_matcher_include_regexp.rb
@@ -1,5 +1,7 @@
 RSpec::Matchers.define :include_regexp do |expected|
   match do |actual|
-    ! actual.grep(expected).empty?
+    warn('The use of "include_regexp"-matchers is deprecated. It will be removed soon.')
+
+    !actual.grep(expected).empty?
   end
 end
