cc9ee221e10ab6a3603d089711d146bc237dd980
Dennis Günnewig <dg1@ratiodata.de>
2015-05-07 14:09:48 +0200

    Check if directory has sub-directory

--- spec/aruba/matchers/directory_spec.rb
+++ spec/aruba/matchers/directory_spec.rb
@@ -23,4 +23,36 @@ RSpec.describe 'Directory Matchers' do
       it { expect(name).not_to be_existing_directory }
     end
   end
+
+  describe 'to_have_sub_directory' do
+    let(:name) { 'test.d' }
+    let(:path) { File.join(@aruba.current_directory, name) }
+    let(:content) { %w(subdir.1.d subdir.2.d) }
+
+    context 'when directory exists' do
+      before :each do
+        FileUtils.mkdir_p path
+      end
+
+      before :each do
+        Array(content).each { |p| Dir.mkdir File.join(path, p) }
+      end
+
+      context 'when single directory' do
+        it { expect(name).to have_sub_directory('subdir.1.d') }
+      end
+
+      context 'when multiple directories' do
+        it { expect(name).to have_sub_directory(['subdir.1.d', 'subdir.2.d']) }
+      end
+
+      context 'when non existing directory' do
+        it { expect(name).not_to have_sub_directory('subdir.3.d') }
+      end
+    end
+
+    context 'when directory does not exist' do
+      it { expect(name).not_to have_sub_directory('subdir') }
+    end
+  end
 end
