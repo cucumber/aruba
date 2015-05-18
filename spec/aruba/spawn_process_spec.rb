17e4012f513fe9382df71ba3e52270cbe3e45bbe
Max Meyer <dev@fedux.org>
2015-05-18 22:22:48 +0200

    Improve specs for spawn_process

--- spec/aruba/spawn_process_spec.rb
+++ spec/aruba/spawn_process_spec.rb
@@ -1,42 +1,63 @@
 require 'aruba/spawn_process'
 
-module Aruba
-  describe SpawnProcess do
-    let(:process) { SpawnProcess.new('echo "yo"', 1, 1) }
+describe Aruba::SpawnProcess do
+  subject(:process) { described_class.new(command, exit_timeout, io_wait, working_directory) }
 
-    describe "#stdout" do
-      before { process.run! }
+  let(:command) { 'echo "yo"' }
+  let(:exit_timeout) { 1 }
+  let(:io_wait) { 1 }
+  let(:working_directory) { Dir.getwd }
 
-      it "returns the stdout" do
-        expect(process.stdout).to eq "yo\n"
-      end
+  describe "#stdout" do
+    before(:each) { process.run! }
 
-      it "returns all the stdout, every time you call it" do
-        expect(process.stdout).to eq "yo\n"
-        expect(process.stdout).to eq "yo\n"
-      end
+    context 'when invoked once' do
+      it { expect(process.stdout).to eq "yo\n" }
     end
 
-    describe "#stop" do
-      before { process.run! }
+    context 'when invoked twice' do
+      it { 2.times { expect(process.stdout).to eq "yo\n" } }
+    end
+  end
 
-      it "sends any output to the reader" do
-        reader = double( 'null_object' )
-        allow( reader ).to receive( :stderr )
-        expect( reader ).to receive( :stdout ).with("yo\n")
+  describe "#stderr" do
+    let(:command) { 'features/fixtures/spawn_process/stderr.sh yo' }
 
-        process.stop(reader)
-      end
+    before(:each) { process.run! }
+
+    context 'when invoked once' do
+      it { expect(process.stderr).to eq "yo\n" }
     end
 
-    describe "#run!" do
-      context "upon process launch error" do
-        let(:process_failure) { SpawnProcess.new('does_not_exists', 1, 1) }
+    context 'when invoked twice' do
+      it { 2.times { expect(process.stderr).to eq "yo\n" } }
+    end
+  end
 
-        it "raises a Aruba::LaunchError" do
-          expect{process_failure.run!}.to raise_error(::Aruba::LaunchError)
-        end
-      end
+  describe "#stop" do
+    let(:reader) { double('reader') }
+
+    before(:each) { process.run! }
+
+    before :each do
+      allow(reader).to receive(:stderr)
+      expect(reader).to receive(:stdout).with("yo\n")
+    end
+
+    context 'when stopped successfully' do
+      it { process.stop(reader) }
+    end
+  end
+
+  describe "#run!" do
+    context "when process run succeeds" do
+      it { expect { process.run! }.not_to raise_error }
+    end
+
+    context "when process run fails" do
+      let(:command) { 'does_not_exists' }
+
+      it { expect {process.run!}.to raise_error Aruba::LaunchError }
     end
   end
 end
