26efeb71212466ef2707295e0aadae9b93eefcda
Max Meyer <dev@fedux.org>
2015-07-03 21:32:22 +0200

    Use equality here

--- spec/aruba/rspec_spec.rb
+++ spec/aruba/rspec_spec.rb
@@ -5,11 +5,11 @@ RSpec.describe 'RSpec Integration', type: :aruba do
     subject(:config) { aruba.config }
 
     context 'when io_wait_timeout is 0.5', io_wait_timeout: 0.5 do
-      it { expect(config.io_wait_timeout).to be 0.5 }
+      it { expect(config.io_wait_timeout).to eq 0.5 }
     end
 
     context 'when io_wait_timeout is 0.1' do
-      it { expect(config.io_wait_timeout).to be 0.1 }
+      it { expect(config.io_wait_timeout).to eq 0.1 }
     end
   end
 end
