4850eb107ec88a786d89aba22e6d1263ea315222
Dennis Günnewig <dg1@ratiodata.de>
2015-07-09 13:24:43 +0200

    Make it possible to use oldstyle main_class

--- lib/aruba/in_process.rb
+++ lib/aruba/in_process.rb
@@ -1,11 +1,18 @@
 require 'aruba/processes/in_process'
+require 'aruba/platform'
 
 module Aruba
   class InProcess < Aruba::Processes::InProcess
     def initialize(*args)
-      warn('The use of "Aruba::InProcess" is deprecated. Use "Aruba::Processes::InProcess" instead.')
+      Aruba::Platform.deprecated('The use of "Aruba::InProcess" is deprecated. Use "Aruba::Processes::InProcess" instead.')
 
       super
+    end
+
+    def self.main_class(*args)
+      Aruba::Platform.deprecated('The use of "Aruba::InProcess" is deprecated. Use "Aruba::Processes::InProcess" instead.')
+
+      Aruba::Processes::InProcess.main_class(*args)
     end
   end
 end
