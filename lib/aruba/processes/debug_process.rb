660f50569e691573d7edc583afa18480cd5bd313
Max Meyer <dev@fedux.org>
2015-07-24 17:38:34 +0200

    Fixed offenses

--- lib/aruba/processes/debug_process.rb
+++ lib/aruba/processes/debug_process.rb
@@ -21,25 +21,42 @@ module Aruba
       # rubocop:enable Metrics/CyclomaticComplexity
       # rubocop:enable Metrics/MethodLength
 
+      # Return stdin
+      #
+      # @return [NilClass]
+      #   Nothing
       def stdin(*); end
 
+      # Return stdout
+      #
+      # @return [String]
+      #   A predefined string to make users aware they are using the DebugProcess
       def stdout(*)
         'This is the debug launcher on STDOUT. If this output is unexpected, please check your setup.'
       end
 
+      # Return stderr
+      #
+      # @return [String]
+      #   A predefined string to make users aware they are using the DebugProcess
       def stderr(*)
         'This is the debug launcher on STDERR. If this output is unexpected, please check your setup.'
       end
 
+      # Write to nothing
       def write(*); end
+
+      # Close nothing
       def close_io(*); end
 
+      # Stop process
       def stop(_reader)
         @stopped = true
 
         @exit_status
       end
 
+      # Terminate process
       def terminate(*)
         stop
       end
