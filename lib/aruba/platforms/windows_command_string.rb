ed5c64e9fbd3e83006294c008d3da13732d208b7
Max Meyer <dev@fedux.org>
2015-08-09 14:26:43 +0200

    Use absolute path for cmd.exe as well

--- lib/aruba/platforms/windows_command_string.rb
+++ lib/aruba/platforms/windows_command_string.rb
@@ -5,7 +5,7 @@ module Aruba
     # This is a command which should be run
     class WindowsCommandString < SimpleDelegator
       def initialize(cmd)
-        __setobj__ 'cmd.exe /c ' + cmd
+        __setobj__ format('%s /c "%s"', Aruba.platform.which('cmd.exe'), cmd)
       end
 
       if RUBY_VERSION < '1.9'
