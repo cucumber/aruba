ce240754d20d04c0204b5e52fc861c8c76835883
Dennis Günnewig <dg1@ratiodata.de>
2015-08-19 10:26:46 +0200

    Make steps for testing frameworks first class citizens

---
+++ lib/aruba/cucumber/testing_frameworks.rb
@@ -0,0 +1,95 @@
+# Cucumber
+Then /^the feature(?:s)? should( not)?(?: all)? pass$/ do |negated|
+  if negated
+    step 'the output should contain " failed)"'
+    step 'the exit status should be 1'
+  else
+    step 'the output should not contain " failed)"'
+    step 'the output should not contain " undefined)"'
+    step 'the exit status should be 0'
+  end
+end
+
+# Cucumber
+Then /^the feature(?:s)? should( not)?(?: all)? pass with( regex)?:$/ do |negated, regex, string|
+  if negated
+    step 'the output should contain " failed)"'
+    step 'the exit status should be 1'
+  else
+    step 'the output should not contain " failed)"'
+    step 'the output should not contain " undefined)"'
+    step 'the exit status should be 0'
+  end
+
+  if regex
+    step "the output should match %r<#{string}>"
+  else
+    step 'the output should contain:', string
+  end
+end
+
+# RSpec
+Then /^the spec(?:s)? should( not)?(?: all)? pass(?: with (\d+) failures?)?$/ do |negated, count_failures|
+  if negated
+    if count_failures.nil?
+      step 'the output should not contain "0 failures"'
+    else
+      step %(the output should contain "#{count_failures} failures")
+    end
+
+    step 'the exit status should be 1'
+  else
+    step 'the output should contain "0 failures"'
+    step 'the exit status should be 0'
+  end
+end
+
+# RSpec
+Then /^the spec(?:s)? should( not)?(?: all)? pass with( regex)?:$/ do |negated, regex, string|
+  if negated
+    step 'the output should contain " failed)"'
+    step 'the exit status should be 1'
+  else
+    step 'the output should not contain " failed)"'
+    step 'the exit status should be 0'
+  end
+
+  if regex
+    step "the output should match %r<#{string}>"
+  else
+    step 'the output should contain:', string
+  end
+end
+
+# Minitest
+Then /^the tests(?:s)? should( not)?(?: all)? pass(?: with (\d+) failures?)?$/ do |negated, count_failures|
+  if negated
+    if count_failures.nil?
+      step 'the output should not contain "0 errors"'
+    else
+      step %(the output should contain "#{count_failures} errors")
+    end
+
+    step 'the exit status should be 1'
+  else
+    step 'the output should contain "0 errors"'
+    step 'the exit status should be 0'
+  end
+end
+
+# Minitest
+Then /^the test(?:s)? should( not)?(?: all)? pass with( regex)?:$/ do |negated, regex, string|
+  if negated
+    step 'the output should contain "0 errors"'
+    step 'the exit status should be 1'
+  else
+    step 'the output should not contain "0 errors"'
+    step 'the exit status should be 0'
+  end
+
+  if regex
+    step "the output should match %r<#{string}>"
+  else
+    step 'the output should contain:', string
+  end
+end
