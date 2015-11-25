fe7ce90eceee231b807aace384604d45a7afcdc4
Dennis Günnewig <dg1@ratiodata.de>
2015-11-25 11:35:25 +0100

    Run skip_invoke on scenario

--- features/step_definitions/hooks.rb
+++ features/step_definitions/hooks.rb
@@ -1,95 +1,95 @@
 require 'cucumber/platform'
 
-Before '@requires-ruby-version-193' do
+Before '@requires-ruby-version-193' do |scenario|
   next if RUBY_VERSION >= '1.9.3'
 
   if Cucumber::VERSION < '2'
-    skip_invoke!
+    scenario.skip_invoke!
   else
     skip_this_scenario
   end
 end
 
-Before '@requires-ruby-version-19' do
+Before '@requires-ruby-version-19' do |scenario|
   next if RUBY_VERSION >= '1.9'
 
   if Cucumber::VERSION < '2'
-    skip_invoke!
+    scenario.skip_invoke!
   else
     skip_this_scenario
   end
 end
 
-Before '@requires-ruby-version-2' do
+Before '@requires-ruby-version-2' do |scenario|
   next if RUBY_VERSION >= '2'
 
   if Cucumber::VERSION < '2'
-    skip_invoke!
+    scenario.skip_invoke!
   else
     skip_this_scenario
   end
 end
 
-Before '@requires-aruba-version-1' do
+Before '@requires-aruba-version-1' do |scenario|
   next if Aruba::VERSION > '1'
 
   if Cucumber::VERSION < '2'
-    skip_invoke!
+    scenario.skip_invoke!
   else
     skip_this_scenario
   end
 end
 
-Before '@requires-ruby-platform-java' do
+Before '@requires-ruby-platform-java' do |scenario|
   # leave if java
   next if RUBY_PLATFORM.include? 'java'
 
   if Cucumber::VERSION < '2'
-    skip_invoke!
+    scenario.skip_invoke!
   else
     skip_this_scenario
   end
 end
 
-Before '@requires-ruby-platform-mri' do
+Before '@requires-ruby-platform-mri' do |scenario|
   # leave if not java
   next unless RUBY_PLATFORM.include? 'java'
 
   if Cucumber::VERSION < '2'
-    skip_invoke!
+    scenario.skip_invoke!
   else
     skip_this_scenario
   end
 end
 
-Before '@unsupported-on-platform-windows' do
+Before '@unsupported-on-platform-windows' do |scenario|
   # leave if not windows
   next unless FFI::Platform.windows?
 
   if Cucumber::VERSION < '2'
-    skip_invoke!
+    scenario.skip_invoke!
   else
     skip_this_scenario
   end
 end
 
-Before '@unsupported-on-platform-unix' do
+Before '@unsupported-on-platform-unix' do |scenario|
   # leave if not windows
   next unless FFI::Platform.unix?
 
   if Cucumber::VERSION < '2'
-    skip_invoke!
+    scenario.skip_invoke!
   else
     skip_this_scenario
   end
 end
 
-Before '@unsupported-on-platform-mac' do
+Before '@unsupported-on-platform-mac' do |scenario|
   # leave if not windows
   next unless FFI::Platform.mac?
 
   if Cucumber::VERSION < '2'
-    skip_invoke!
+    scenario.skip_invoke!
   else
     skip_this_scenario
   end
