121f4d1c9ccd02ddd8424bfcef29b9d76f493e58
Dennis Günnewig <dg1@ratiodata.de>
2016-01-04 11:50:19 +0100

    Added missing announcer

--- lib/aruba/setup.rb
+++ lib/aruba/setup.rb
@@ -37,6 +37,7 @@ module Aruba
           runtime.announcer.announce :command, event.entity.commandline
           runtime.announcer.announce :timeout, 'exit', event.entity.exit_timeout
           runtime.announcer.announce :timeout, 'io wait', event.entity.io_wait_timeout
+          runtime.announcer.announce :wait_time, 'startup wait time', event.entity.startup_wait_time
           runtime.announcer.announce :full_environment, event.entity.environment
         end
       )
