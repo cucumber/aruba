397d1721922c9754699435cd1fed7555276b4e39
Dennis Günnewig <dg1@ratiodata.de>
2016-01-26 08:53:33 +0100

    Only needed defaults

--- spec/support/configs/aruba.rb
+++ spec/support/configs/aruba.rb
@@ -1,7 +1,5 @@
 require 'aruba/rspec'
 
 Aruba.configure do |config|
-  config.exit_timeout                          = 120
-  config.io_wait_timeout                       = 2
   config.activate_announcer_on_command_failure = [:stderr, :stdout, :command]
 end
