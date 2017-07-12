6effd27046355c95a5b9c9a7871506139f6d444c
Dennis Günnewig <dev@fedux.org>
2017-07-12 21:10:55 +0200

    Move specs for #with_environment

--- spec/aruba/api_spec.rb
+++ spec/aruba/api_spec.rb
@@ -345,63 +345,6 @@ describe Aruba::Api do
     end #with_file_content
   end
 
-  describe 'process environment' do
-    context '#with_environment' do
-      it 'modifies env for block' do
-        variable = 'THIS_IS_A_ENV_VAR'
-        ENV[variable] = '1'
-
-        @aruba.with_environment variable => '0' do
-          expect(ENV[variable]).to eq '0'
-        end
-
-        expect(ENV[variable]).to eq '1'
-      end
-
-      it 'works together with #set_environment_variable' do
-        variable = 'THIS_IS_A_ENV_VAR'
-        @aruba.set_environment_variable variable, '1'
-
-        @aruba.with_environment do
-          expect(ENV[variable]).to eq '1'
-          @aruba.set_environment_variable variable, '0'
-          @aruba.with_environment do
-            expect(ENV[variable]).to eq '0'
-          end
-          expect(ENV[variable]).to eq '1'
-        end
-      end
-
-      it 'works with a mix of ENV and #set_environment_variable' do
-        variable = 'THIS_IS_A_ENV_VAR'
-        @aruba.set_environment_variable variable, '1'
-        ENV[variable] = '2'
-        expect(ENV[variable]).to eq '2'
-
-        @aruba.with_environment do
-          expect(ENV[variable]).to eq '1'
-          @aruba.set_environment_variable variable, '0'
-          @aruba.with_environment do
-            expect(ENV[variable]).to eq '0'
-          end
-          expect(ENV[variable]).to eq '1'
-        end
-        expect(ENV[variable]).to eq '2'
-      end
-
-      it 'keeps values not set in argument' do
-        variable = 'THIS_IS_A_ENV_VAR'
-        ENV[variable] = '2'
-        expect(ENV[variable]).to eq '2'
-
-        @aruba.with_environment do
-          expect(ENV[variable]).to eq '2'
-        end
-        expect(ENV[variable]).to eq '2'
-      end
-    end
-  end
-
   describe 'tags' do
     describe '@announce_stdout' do
       after(:each) { @aruba.all_commands.each(&:stop) }
