110044e9ce0465128841770751a3280fca3b797d
Dennis Günnewig <dev@fedux.org>
2017-07-12 19:20:14 +0200

    Move specs for #all_files

--- spec/aruba/api_spec.rb
+++ spec/aruba/api_spec.rb
@@ -7,34 +7,6 @@ describe Aruba::Api do
   include_context 'uses aruba API'
 
 
-  describe '#all_files' do
-    let(:name) { @file_name }
-    let(:path) { @file_path }
-
-    context 'when file exist' do
-      before :each do
-        Aruba.platform.write_file(path, '')
-      end
-
-      it { expect(all_files).to include expand_path(name) }
-    end
-
-    context 'when directory exist' do
-      let(:name) { 'test_dir' }
-      let(:path) { File.join(@aruba.aruba.current_directory, name) }
-
-      before :each do
-        Aruba.platform.mkdir(path)
-      end
-
-      it { expect(all_files).to eq [] }
-    end
-
-    context 'when nothing exist' do
-      it { expect(all_files).to eq [] }
-    end
-  end
-
   describe '#all_directories' do
     let(:name) { @file_name }
     let(:path) { @file_path }
