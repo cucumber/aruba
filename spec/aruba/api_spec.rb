111dc38f203db1b94eb862da43b90732d5b3ad35
Dennis Günnewig <dev@fedux.org>
2017-07-12 19:50:42 +0200

    Move specs for #directory?

--- spec/aruba/api_spec.rb
+++ spec/aruba/api_spec.rb
@@ -7,42 +7,6 @@ describe Aruba::Api do
   include_context 'uses aruba API'
 
   describe 'files' do
-    describe '#directory?' do
-      context 'when is file' do
-        let(:name) { @file_name }
-        let(:path) { @file_path }
-
-        context 'when exists' do
-          before :each do
-            Aruba.platform.write_file(path, '')
-          end
-
-          it { expect(@aruba).not_to be_directory(name) }
-        end
-
-        context 'when does not exist' do
-          it { expect(@aruba).not_to be_directory(name) }
-        end
-      end
-
-      context 'when is directory' do
-        let(:name) { 'test.d' }
-        let(:path) { File.join(@aruba.aruba.current_directory, name) }
-
-        context 'when exists' do
-          before :each do
-            Aruba.platform.mkdir(path)
-          end
-
-          it { expect(@aruba).to be_directory(name) }
-        end
-
-        context 'when does not exist' do
-          it { expect(@aruba).not_to be_directory(name) }
-        end
-      end
-    end
-
     describe '#copy' do
       let(:source) { 'file.txt' }
       let(:destination) { 'file1.txt' }
