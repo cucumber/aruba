3fe37c7cf8e3bb95a68611a83d61fe0e5f38a51b
Matijs van Zuijlen <matijs@matijs.net>
2017-09-03 11:48:12 +0200

    Change #check_filesystem_permissions specs to #have_permissions

--- spec/aruba/api_spec.rb
+++ spec/aruba/api_spec.rb
@@ -1,5 +1,4 @@
 require 'spec_helper'
-require 'securerandom'
 require 'aruba/api'
 require 'fileutils'
 
@@ -831,67 +830,6 @@ describe Aruba::Api do
           let(:file_path) { File.join(@aruba.aruba.current_directory, path) }
 
           it { expect(actual_permissions).to eq('0644') }
-        end
-      end
-    end
-
-    describe '#check_filesystem_permissions' do
-      let(:file_name) { @file_name }
-      let(:file_path) { @file_path }
-
-      let(:permissions) { '0644' }
-
-      before :each do
-        @aruba.set_environment_variable 'HOME', File.expand_path(@aruba.aruba.current_directory)
-      end
-
-      before(:each) do
-        File.open(file_path, 'w') { |f| f << "" }
-      end
-
-      before(:each) do
-        @aruba.filesystem_permissions(permissions, file_name)
-      end
-
-      context 'when file exists' do
-        context 'and should have permissions' do
-          context 'and permissions are given as string' do
-            it { @aruba.check_filesystem_permissions(permissions, file_name, true) }
-          end
-
-          context 'and permissions are given as octal number' do
-            let(:permissions) { 0o666 }
-
-            it { @aruba.check_filesystem_permissions(permissions, file_name, true) }
-          end
-
-          context 'and path includes ~' do
-            let(:string) { random_string }
-            let(:file_name) { File.join('~', string) }
-            let(:file_path) { File.join(@aruba.aruba.current_directory, string) }
-
-            it { @aruba.check_filesystem_permissions(permissions, file_name, true) }
-          end
-
-          context 'but fails because the permissions are different' do
-            let(:expected_permissions) { 0o666 }
-
-            it { expect { @aruba.check_filesystem_permissions(expected_permissions, file_name, true) }.to raise_error }
-          end
-        end
-
-        context 'and should not have permissions' do
-          context 'and succeeds when the difference is expected and permissions are different' do
-            let(:different_permissions) { 0o666 }
-
-            it { @aruba.check_filesystem_permissions(different_permissions, file_name, false) }
-          end
-
-          context 'and fails because the permissions are the same although they should be different' do
-            let(:different_permissions) { 0o644 }
-
-            it { expect { @aruba.check_filesystem_permissions(different_permissions, file_name, false) }.to raise_error }
-          end
         end
       end
     end
--- spec/aruba/matchers/path_spec.rb
+++ spec/aruba/matchers/path_spec.rb
@@ -85,4 +85,65 @@ RSpec.describe 'Path Matchers' do
       end
     end
   end
+
+  describe 'to have_permissions' do
+    let(:file_name) { @file_name }
+    let(:file_path) { @file_path }
+
+    let(:permissions) { '0644' }
+
+    before :each do
+      @aruba.set_environment_variable 'HOME', File.expand_path(@aruba.aruba.current_directory)
+    end
+
+    before(:each) do
+      File.open(file_path, 'w') { |f| f << "" }
+    end
+
+    before(:each) do
+      @aruba.chmod(permissions, file_name)
+    end
+
+    context 'when file exists' do
+      context 'and should have permissions' do
+        context 'and permissions are given as string' do
+          it { expect(file_name).to have_permissions permissions }
+        end
+
+        context 'and permissions are given as octal number' do
+          let(:permissions) { 0o666 }
+
+          it { expect(file_name).to have_permissions permissions }
+        end
+
+        context 'and path includes ~' do
+          let(:string) { random_string }
+          let(:file_name) { File.join('~', string) }
+          let(:file_path) { File.join(@aruba.aruba.current_directory, string) }
+
+          it { expect(file_name).to have_permissions permissions }
+        end
+
+        context 'but fails because the permissions are different' do
+          let(:expected_permissions) { 0o666 }
+
+          it { expect { expect(file_name).to have_permissions expected_permissions }.to raise_error }
+        end
+      end
+
+      context 'and should not have permissions' do
+        context 'and succeeds when the difference is expected and permissions are different' do
+          let(:different_permissions) { 0o666 }
+
+          it { expect(file_name).not_to have_permissions different_permissions }
+        end
+
+        context 'and fails because the permissions are the same although they should be different' do
+          let(:different_permissions) { 0o644 }
+
+          it { expect { expect(file_name).not_to have_permissions different_permissions }.to raise_error }
+        end
+      end
+    end
+  end
 end
