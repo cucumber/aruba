3ad0d12ffdd76862407b73b1b5054c869630e42c
Matijs van Zuijlen <matijs@matijs.net>
2017-09-03 15:11:47 +0200

    Remove #match_path_pattern matcher and its specs

--- spec/aruba/matchers/path_spec.rb
+++ spec/aruba/matchers/path_spec.rb
@@ -10,36 +10,6 @@ RSpec.describe 'Path Matchers' do
     @aruba.expand_path(*args)
   end
 
-  describe 'to_match_path_pattern' do
-    context 'when pattern is string' do
-      context 'when there is file which matches path pattern' do
-        before :each do
-          Aruba.platform.write_file(@file_path, '')
-        end
-
-        it { expect(all_paths).to match_path_pattern(expand_path(@file_name)) }
-      end
-
-      context 'when there is not file which matches path pattern' do
-        it { expect(all_paths).not_to match_path_pattern('test') }
-      end
-    end
-
-    context 'when pattern is regex' do
-      context 'when there is file which matches path pattern' do
-        before :each do
-          Aruba.platform.write_file(@file_path, '')
-        end
-
-        it { expect(all_paths).to match_path_pattern(/test/) }
-      end
-
-      context 'when there is not file which matches path pattern' do
-        it { expect(all_paths).not_to match_path_pattern(/test/) }
-      end
-    end
-  end
-
   describe 'to_be_an_absolute_path' do
     let(:name) { @file_name }
     let(:path) { @aruba.expand_path(name) }
