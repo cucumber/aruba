83cdedce5ac9ff009f63e7e6304113a20245e7be
Matijs van Zuijlen <matijs@matijs.net>
2017-11-22 20:41:15 +0100

    Improve specs
    - Merge before blocks and remove :each
    - Move calls that are part of the specs' Act steps into the it blocks
    - Replace context blocks with describe and name the correct method names

--- spec/aruba/command_spec.rb
+++ spec/aruba/command_spec.rb
@@ -24,67 +24,62 @@ RSpec.describe Aruba::Command do
   let(:stop_signal) { nil }
   let(:startup_wait_time) { 1 }
 
-  context '#started' do
-    before :each do
+  describe '#start' do
+    before do
       allow(event_bus).to receive(:notify).with(Events::CommandStarted)
     end
 
-    before :each do
-      command.start
-    end
-
     it 'leaves the command in the started state' do
+      command.start
       expect(command).to be_started
     end
   end
 
-  context '#stopped' do
-    before :each do
+  describe '#stop' do
+    before do
       allow(event_bus).to receive(:notify).with(Events::CommandStarted)
       allow(event_bus).to receive(:notify).with(Events::CommandStopped)
-    end
-
-    before :each do
       command.start
-      command.stop
     end
 
     it 'leaves the command in the stopped state' do
+      command.stop
       expect(command).to be_stopped
     end
 
     it 'notifies the event bus only once per run' do
+      command.stop
       command.stop
       expect(event_bus).to have_received(:notify).with(Events::CommandStopped).once
     end
 
     it 'prevents #terminate from notifying the event bus' do
+      command.stop
       command.terminate
       expect(event_bus).to have_received(:notify).with(Events::CommandStopped).once
     end
   end
 
-  context '#terminate' do
-    before :each do
+  describe '#terminate' do
+    before do
       allow(event_bus).to receive(:notify).with(Events::CommandStarted)
       allow(event_bus).to receive(:notify).with(Events::CommandStopped)
-    end
-
-    before :each do
       command.start
-      command.terminate
     end
 
     it 'leaves the command in the stopped state' do
+      command.terminate
       expect(command).to be_stopped
     end
 
     it 'notifies the event bus only once per run' do
       command.terminate
+      command.terminate
       expect(event_bus).to have_received(:notify).with(Events::CommandStopped).once
     end
 
     it 'prevents #stop from notifying the event bus' do
+      command.terminate
       command.stop
       expect(event_bus).to have_received(:notify).with(Events::CommandStopped).once
     end
