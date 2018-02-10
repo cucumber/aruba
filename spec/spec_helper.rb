66ed5ac6aef7cf54da84ae996a92b2e3d0e03e2d
Matijs van Zuijlen <matijs@matijs.net>
2018-02-10 07:16:41 +0100

    Improve spec isolation by not blanket-including Aruba

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -18,6 +18,3 @@ else
   Dir.glob(::File.expand_path('../support/*.rb', __FILE__)).each { |f| require_relative f }
   Dir.glob(::File.expand_path('../support/**/*.rb', __FILE__)).each { |f| require_relative f }
 end
-
-# Avoid writing "describe LocalPac::MyClass do [..]" but "describe MyClass do [..]"
-include Aruba
