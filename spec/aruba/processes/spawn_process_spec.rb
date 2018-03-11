330fb905c6e16389092c37624f083c2a5e2c7351
Matijs van Zuijlen <matijs@matijs.net>
2018-03-11 13:18:38 +0100

    Clean up spec for ChildProcess launch error
    This splits the specs in a Unix and Windows versions, just like for the
    specs for commands with spaces.

--- spec/aruba/processes/spawn_process_spec.rb
+++ spec/aruba/processes/spawn_process_spec.rb
@@ -70,37 +70,48 @@ RSpec.describe Aruba::Processes::SpawnProcess do
       it { expect { process.start }.to raise_error Aruba::LaunchError }
     end
 
-    context "with a childprocess launch error" do
-      let(:child) { instance_double(ChildProcess::AbstractProcess) }
+    context 'with a childprocess launch error on unix' do
+      let(:child) { instance_double(ChildProcess::AbstractProcess).as_null_object }
+      let(:io) { instance_double(ChildProcess::AbstractIO).as_null_object }
       let(:command) { 'foo' }
+      let(:command_path) { '/bar/foo' }
 
       before do
-        allow(ChildProcess).to receive(:build).and_return(child)
+        allow(Aruba.platform).to receive(:command_string).and_return Aruba::Platforms::UnixCommandString
+        allow(Aruba.platform).to receive(:which).with(command, anything).and_return(command_path)
+        allow(ChildProcess).to receive(:build).with(command_path).and_return(child)
 
-        # STEP 1: Simlulate the program exists (but will fail to run, e.g. EACCESS?)
-        allow(Aruba.platform).to receive(:which).with(command, anything).and_return("/foo")
-
-        # STEP 2: Simulate the failure on Windows
         allow(child).to receive(:start).and_raise(ChildProcess::LaunchError, "Foobar!")
-
-        # TODO: wrap the result of ChildProcess.build with a special Aruba
-        # class , so the remaining mocks below won't be needed
-        allow(child).to receive(:leader=)
-
-        io = instance_double(ChildProcess::AbstractIO)
-        allow(io).to receive(:stdout=)
-        allow(io).to receive(:stderr=)
-
         allow(child).to receive(:io).and_return(io)
-
-        allow(child).to receive(:duplex=)
-        allow(child).to receive(:cwd=)
-
         allow(child).to receive(:environment).and_return({})
       end
 
       it "reraises LaunchError as Aruba's LaunchError" do
-        expect { process.start }.to raise_error(Aruba::LaunchError, "It tried to start foo. Foobar!")
+        expect { process.start }.to raise_error(Aruba::LaunchError, "It tried to start #{command}. Foobar!")
+      end
+    end
+
+    context 'with a childprocess launch error on windows' do
+      let(:child) { instance_double(ChildProcess::AbstractProcess).as_null_object }
+      let(:io) { instance_double(ChildProcess::AbstractIO).as_null_object }
+      let(:command) { 'foo' }
+      let(:cmd_path) { 'C:\Bar\cmd.exe' }
+      let(:command_path) { 'D:\Foo\foo' }
+
+      before do
+        allow(Aruba.platform).to receive(:command_string).and_return Aruba::Platforms::WindowsCommandString
+        allow(Aruba.platform).to receive(:which).with('cmd.exe').and_return(cmd_path)
+        allow(Aruba.platform).to receive(:which).with(command, anything).and_return(command_path)
+        allow(ChildProcess).to receive(:build).with(cmd_path, '/c', command_path).and_return(child)
+
+        allow(child).to receive(:start).and_raise(ChildProcess::LaunchError, "Foobar!")
+
+        allow(child).to receive(:io).and_return(io)
+        allow(child).to receive(:environment).and_return({})
+      end
+
+      it "reraises LaunchError as Aruba's LaunchError" do
+        expect { process.start }.to raise_error(Aruba::LaunchError, "It tried to start #{command}. Foobar!")
       end
     end
 
