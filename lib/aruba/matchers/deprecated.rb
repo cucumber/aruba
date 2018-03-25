041de416a5013c594f7cf5540d1b9b4cc51ce219
Andrew Walter <andrew.walter@burnet.edu.au>
2018-03-25 18:29:39 +1100

    Refactored a little.

--- lib/aruba/matchers/deprecated.rb
+++ lib/aruba/matchers/deprecated.rb
@@ -1,15 +1,1 @@
-module DeprecatedMatchers
-  def have_same_file_content_like(expected)
-    RSpec.deprecate('`have_same_file_content_like`', replacement: '`have_same_file_content_as`')
-
-    have_same_file_content_as(expected)
-  end
-
-  def a_file_with_same_content_like(expected)
-    RSpec.deprecate('`a_file_with_same_content_like`', replacement: '`a_file_with_same_content_as`')
-
-    a_file_with_same_content_as(expected)
-  end
-end
-
-RSpec::Matchers.send(:include, DeprecatedMatchers)
+Aruba.platform.require_matching_files('../deprecated/**/*.rb', __FILE__)
---
+++ lib/aruba/matchers/deprecated/file.rb
@@ -0,0 +1,11 @@
+def have_same_file_content_like(expected)
+  RSpec.deprecate('`have_same_file_content_like`', :replacement => '`have_same_file_content_as`')
+
+  have_same_file_content_as(expected)
+end
+
+def a_file_with_same_content_like(expected)
+  RSpec.deprecate('`a_file_with_same_content_like`', :replacement => '`a_file_with_same_content_as`')
+
+  a_file_with_same_content_as(expected)
+end
