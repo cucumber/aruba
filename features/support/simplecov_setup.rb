9359aa5d139dcc2976187e4bd36196e1c9f24f84
Luke Hill <lukehill_uk@hotmail.com>
2019-07-01 13:11:35 +0200

    When using expand path, use a slightly cleaner reference according to rubocop

--- features/support/simplecov_setup.rb
+++ features/support/simplecov_setup.rb
@@ -1,7 +1,7 @@
 # @note this file is loaded in env.rb to setup simplecov using RUBYOPTs for child processes and @in-process
 require 'simplecov'
 
-root = File.expand_path('../../../', __FILE__)
+root = File.expand_path('../..', __dir__)
 
 SimpleCov.command_name(ENV['SIMPLECOV_COMMAND_NAME'])
 SimpleCov.root(root)
