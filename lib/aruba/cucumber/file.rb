d40148cf3906b1248e4e7f41d2f9f173945bec9c
Matijs van Zuijlen <matijs@matijs.net>
2019-07-20 10:51:38 +0200

    Allow both 'a' and 'the' in step, as documented

--- lib/aruba/cucumber/file.rb
+++ lib/aruba/cucumber/file.rb
@@ -180,7 +180,7 @@ Then(/^(?:a|the) file(?: named)? "([^"]*)" should (not )?be equal to file "([^"]
   end
 end
 
-Then(/^the (?:file|directory)(?: named)? "([^"]*)" should( not)? have permissions "([^"]*)"$/) do |path, negated, permissions|
+Then(/^(?:a|the) (?:file|directory)(?: named)? "([^"]*)" should( not)? have permissions "([^"]*)"$/) do |path, negated, permissions|
   if negated
     expect(path).not_to have_permissions(permissions)
   else
