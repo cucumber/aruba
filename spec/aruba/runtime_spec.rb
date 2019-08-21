1edd9cbd15463170a5e13224dcac02a1498a5678
Luke Hill <lukehill_uk@hotmail.com>
2019-08-21 10:48:15 +0200

    Uncomment specs, fix up linelength and add pending flags for the tests to be fixed up

--- spec/aruba/runtime_spec.rb
+++ spec/aruba/runtime_spec.rb
@@ -1,29 +1,35 @@
 require 'spec_helper'
 
 RSpec.describe Aruba::Runtime do
-  # describe '#fixtures_directory' do
-  #   context 'when no fixtures directories exist' do
-  #     it "should raise exception" do
-  #       expect { api.fixtures_directory }.to raise_error
-  #     end
-  #   end
-  #
-  #   context 'when "/features/fixtures"-directory exist' do
-  #     before(:each) { api.create_directory('features/fixtures') }
-  #
-  #     it { expect(api.fixtures_directory).to eq expand_path('features/fixtures') }
-  #   end
-  #
-  #   context 'when "/spec/fixtures"-directory exist' do
-  #     before(:each) { api.create_directory('spec/fixtures') }
-  #
-  #     it { expect(api.fixtures_directory).to eq expand_path('spec/fixtures') }
-  #   end
-  #
-  #   context 'when "/test/fixtures"-directory exist' do
-  #     before(:each) { api.create_directory('test/fixtures') }
-  #
-  #     it { expect(api.fixtures_directory.to_s).to eq expand_path('test/fixtures') }
-  #   end
-  # end
+  describe '#fixtures_directory' do
+    context 'when no fixtures directories exist' do
+      it "should raise exception" do
+        expect { api.fixtures_directory }.to raise_error
+      end
+    end
+
+    context 'when "/features/fixtures"-directory exist' do
+      it {
+        pending("These tests need fixing and classifying")
+        api.create_directory('features/fixtures')
+        expect(api.fixtures_directory).to eq expand_path('features/fixtures')
+      }
+    end
+
+    context 'when "/spec/fixtures"-directory exist' do
+      it {
+        pending("These tests need fixing and classifying")
+        api.create_directory('spec/fixtures')
+        expect(api.fixtures_directory).to eq expand_path('spec/fixtures')
+      }
+    end
+
+    context 'when "/test/fixtures"-directory exist' do
+      it {
+        pending("These tests need fixing and classifying")
+        api.create_directory('test/fixtures')
+        expect(api.fixtures_directory.to_s).to eq expand_path('test/fixtures')
+      }
+    end
+  end
 end
