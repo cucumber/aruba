f7122609f4e0d5f1a963a22953a088582cdc9758
Luke Hill <lukehill_uk@hotmail.com>
2020-01-03 10:01:17 +0100

    Get rubocop fully passing again

--- spec/aruba/runtime_spec.rb
+++ spec/aruba/runtime_spec.rb
@@ -3,14 +3,14 @@ require 'spec_helper'
 RSpec.describe Aruba::Runtime do
   describe '#fixtures_directory' do
     context 'when no fixtures directories exist' do
-      it "should raise exception" do
+      it 'should raise exception' do
         expect { api.fixtures_directory }.to raise_error
       end
     end
 
     context 'when "/features/fixtures"-directory exist' do
       it {
-        pending("These tests need fixing and classifying")
+        pending('These tests need fixing and classifying')
         api.create_directory('features/fixtures')
         expect(api.fixtures_directory).to eq expand_path('features/fixtures')
       }
@@ -18,7 +18,7 @@ RSpec.describe Aruba::Runtime do
 
     context 'when "/spec/fixtures"-directory exist' do
       it {
-        pending("These tests need fixing and classifying")
+        pending('These tests need fixing and classifying')
         api.create_directory('spec/fixtures')
         expect(api.fixtures_directory).to eq expand_path('spec/fixtures')
       }
@@ -26,7 +26,7 @@ RSpec.describe Aruba::Runtime do
 
     context 'when "/test/fixtures"-directory exist' do
       it {
-        pending("These tests need fixing and classifying")
+        pending('These tests need fixing and classifying')
         api.create_directory('test/fixtures')
         expect(api.fixtures_directory.to_s).to eq expand_path('test/fixtures')
       }
