1a6a4c408abea8e98d42a04ab486776d065216b6
Luke Hill <lukehill_uk@hotmail.com>
2020-01-03 10:14:02 +0100

    Fix up issues that occur in the 145-150LineChar area (Not yet fully complete)

--- lib/aruba/config_wrapper.rb
+++ lib/aruba/config_wrapper.rb
@@ -30,7 +30,9 @@ module Aruba
     # If one method ends with "=", e.g. ":option1=", then notify the event
     # queue, that the user changes the value of "option1"
     def method_missing(name, *args, &block)
-      event_bus.notify Events::ChangedConfiguration.new(changed: { name: name.to_s.gsub(/=$/, ''), value: args.first }) if name.to_s.end_with? '='
+      if name.to_s.end_with? '='
+        event_bus.notify Events::ChangedConfiguration.new(changed: { name: name.to_s.gsub(/=$/, ''), value: args.first })
+      end
 
       config.send(name, *args, &block)
     end
