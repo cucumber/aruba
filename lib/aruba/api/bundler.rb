bc8bffd3b218124094a2ce8c6fd6f3e7d5bcc216
Matijs van Zuijlen <matijs@matijs.net>
2020-01-18 14:28:25 +0100

    Unset variables using Bundler tooling

--- lib/aruba/api/bundler.rb
+++ lib/aruba/api/bundler.rb
@@ -7,8 +7,23 @@ module Aruba
 
       # Unset variables used by bundler
       def unset_bundler_env_vars
-        %w(RUBYOPT BUNDLE_PATH BUNDLE_BIN_PATH BUNDLE_GEMFILE).each do |key|
-          delete_environment_variable(key)
+        empty_env = with_environment { with_unbundled_env { ENV.to_h } }
+        aruba_env = aruba.environment.to_h
+        (aruba_env.keys - empty_env.keys).each do |key|
+          delete_environment_variable key
+        end
+        empty_env.each do |k, v|
+          set_environment_variable k, v
+        end
+      end
+
+      private
+
+      def with_unbundled_env(&block)
+        if ::Bundler.respond_to?(:with_unbundled_env)
+          ::Bundler.with_unbundled_env(&block)
+        else
+          ::Bundler.with_clean_env(&block)
         end
       end
     end
