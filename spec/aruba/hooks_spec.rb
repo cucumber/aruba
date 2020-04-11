315c87180e0057ee52494898be4a86f9757db913
Matijs van Zuijlen <matijs@matijs.net>
2020-04-11 13:01:30 +0200

    Autocorrect Style/Lambda

--- spec/aruba/hooks_spec.rb
+++ spec/aruba/hooks_spec.rb
@@ -3,14 +3,14 @@ require 'spec_helper'
 describe Aruba::Hooks do
   it 'executes a stored hook' do
     hook_was_run = false
-    subject.append :hook_label, lambda { hook_was_run = true }
+    subject.append :hook_label, -> { hook_was_run = true }
     subject.execute :hook_label, self
     expect(hook_was_run).to be_truthy
   end
 
   it 'executes a stored hook that takes multiple arguments' do
     hook_values = []
-    subject.append :hook_label, lambda { |a, b, c| hook_values = [a, b, c] }
+    subject.append :hook_label, ->(a, b, c) { hook_values = [a, b, c] }
     subject.execute :hook_label, self, 1, 2, 3
     expect(hook_values).to eq [1, 2, 3]
   end
