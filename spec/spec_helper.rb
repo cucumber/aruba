0281175bdb1c24fd29a7c1276c9e8ab946b26f71
Matijs van Zuijlen <matijs@matijs.net>
2022-01-07 11:37:39 +0100

    Avoid double start of SimpleCov
    In Ruby 3.1, calling SimpleCov.start twice raises an error. Avoid this
    by removing SimpleCov.start from the .simplecov file.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,8 +2,10 @@ $LOAD_PATH << ::File.expand_path("../lib", __dir__)
 
 unless RUBY_PLATFORM.include?("java")
   require "simplecov"
-  SimpleCov.command_name "rspec"
-  SimpleCov.start
+  SimpleCov.command_name "RSpec"
+
+  # Run simplecov by default
+  SimpleCov.start unless ENV.key? "ARUBA_NO_COVERAGE"
 end
 
 # Loading support files
