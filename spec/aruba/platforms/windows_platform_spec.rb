01f468c61eecc83d931cb56434394770557c60c7
Matijs van Zuijlen <matijs@matijs.net>
2022-01-16 11:29:55 +0100

    Ensure Gem.win_platform? is available
    Even though rubygems is nearly always loaded by ruby automatically, it
    is possible to avoid doing so. Therefore, to make platform detection
    work, we have to require "rubygems" explicitly.

---
+++ spec/aruba/platforms/windows_platform_spec.rb
@@ -0,0 +1,18 @@
+require "spec_helper"
+require "aruba/platforms/windows_platform"
+
+RSpec.describe Aruba::Platforms::WindowsPlatform do
+  include_context "uses aruba API"
+
+  describe ".match?" do
+    it "works even when ruby is launched with --disable-gems and --disable-rubyopt" do
+      aruba_lib = File.expand_path("../../../lib", __dir__)
+      run_command_and_stop(
+        "ruby --disable-rubyopt --disable-gems -I#{aruba_lib}" \
+        " -e 'require \"aruba/platforms/windows_platform\";" \
+        " Aruba::Platforms::WindowsPlatform.match?;'"
+      )
+      expect(last_command_started).to be_successfully_executed
+    end
+  end
+end
