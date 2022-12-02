3657d7db9198970b43f45f78a95ead96afe7d768
Matijs van Zuijlen <matijs@matijs.net>
2022-12-02 13:20:44 +0100

    Improve specs for Aruba::CommandMonitor
    - Improve spec description
    - Add spec for failure mode

--- spec/aruba/platforms/command_monitor_spec.rb
+++ spec/aruba/platforms/command_monitor_spec.rb
@@ -3,16 +3,19 @@ require "spec_helper"
 RSpec.describe Aruba::CommandMonitor do
   let(:monitor) { described_class.new(announcer: announcer) }
   let(:announcer) { instance_double(Aruba::Platforms::Announcer) }
-  let(:process) { instance_double(Aruba::Processes::BasicProcess) }
+  let(:process) { instance_double(Aruba::Processes::BasicProcess, commandline: "foobar") }
 
   before do
-    allow(process).to receive(:commandline).and_return("foobar")
     monitor.register_command(process)
   end
 
   describe "#find" do
-    it "works" do
-      expect(monitor.find("foobar")).to eq(process)
+    it "find the process with the given name" do
+      expect(monitor.find("foobar")).to eq process
+    end
+
+    it "fails if no process with the given name exists" do
+      expect { monitor.find("boofar") }.to raise_error Aruba::CommandNotFoundError
     end
   end
 end
