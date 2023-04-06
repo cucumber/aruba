d44a4646666bf166669a39955497e5d370011569
M. Scott Ford <scott@corgibytes.com>
2023-04-05 21:02:29 -0400

    Restore coverage collection for cucumber features
    Note: `simplecov_setup` has been moved to it's own directory because of the way that it needs to be specified when setting the `RUBYOPTS` environment variable. Just specifying the path to the full file path via `-r` does not work. When doing that an error is generated that the file cannot be found. Instead, the directory containing the file is added to the load path via `-I`, and then the file is required with a simple `-rsimplecov_setup`. Because this modifies the load path, having a file named `aruba.rb` in the same directory as `simplecov_setup.rb` caused problems for files that attempted to `require 'aruba'`.

--- features/support/simplecov/simplecov_setup.rb
+++ features/support/simplecov/simplecov_setup.rb
@@ -2,7 +2,7 @@
 # child processes and @in-process
 unless RUBY_PLATFORM.include?("java")
   require "simplecov"
-  root = File.expand_path("../..", __dir__)
+  root = File.expand_path("../../..", __dir__)
   command_name = ENV["SIMPLECOV_COMMAND_NAME"] || "Cucumber Features"
   SimpleCov.command_name(command_name)
   SimpleCov.root(root)
