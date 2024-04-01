144ad82ce953d246feb505a8061aa319463ae410
Matijs van Zuijlen <matijs@matijs.net>
2024-04-01 13:16:00 +0200

    Clean up file existence assertions in specs
    This replaces uses of the be_exist matcher, and reduces spec nesting as
    well. It also removes shared examples. As a result, these specs should
    now be easier to read.

--- spec/support/shared_examples/directory.rb
+++
@@ -1,7 +0,0 @@
-shared_examples "an existing directory" do
-  it { Array(path).each { |p| expect(File).to be_directory(p) } }
-end
-
-shared_examples "a non-existing directory" do
-  it { Array(path).each { |p| expect(File).not_to be_exist(p) } }
-end
--- spec/support/shared_examples/file.rb
+++
@@ -1,7 +0,0 @@
-shared_examples "an existing file" do
-  it { Array(path).each { |p| expect(File).to be_file(p) } }
-end
-
-shared_examples "a non-existing file" do
-  it { Array(path).each { |p| expect(File).not_to be_exist(p) } }
-end
