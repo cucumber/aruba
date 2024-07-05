fc3109173637302ec012a2cdfb02c646e56f15f4
Matijs van Zuijlen <matijs@matijs.net>
2024-07-05 13:29:45 +0200

    WIP: Note slowdown

--- features/support/simplecov_setup.rb
+++ features/support/simplecov_setup.rb
@@ -2,6 +2,18 @@
 
 # @note this file is loaded in env.rb to setup simplecov using RUBYOPTs for
 # child processes and @in-process
+#
+# Current implementation slows down things immensely:
+#
+# On main:
+# bin/cucumber --format progress  192.97s user 34.46s system 79% cpu 4:47.10 total
+#
+# On fix-simplecov-integration: with SimpleFormatter
+# bin/cucumber --format progress  581.96s user 75.12s system 91% cpu 12:00.38 total
+#
+# On fix-simplecov-integration: with DumbFormatter
+# bin/cucumber --format progress  732.04s user 105.59s system 92% cpu 15:05.49 total
+#
 unless RUBY_PLATFORM.include?("java")
   require "simplecov"
   root = File.expand_path("../..", __dir__)
