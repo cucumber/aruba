fa889f998b45ba077afbff131abc62a77408ac07
Phil Pirozhkov <hello@fili.pp.ru>
2025-09-13 01:31:12 +0300

    Use modern RSpec option
    See https://github.com/rspec/rspec-core/pull/2845

--- spec/support/configs/rspec.rb
+++ spec/support/configs/rspec.rb
@@ -4,9 +4,7 @@ require 'rspec/core'
 require 'aruba/api'
 
 RSpec.configure do |config|
-  config.filter_run focus: true
-
-  config.run_all_when_everything_filtered = true
+  config.filter_run_when_matching :focus
 
   config.example_status_persistence_file_path = 'tmp/rspec-examples.txt'
   config.profile_examples = 10
