266592b5e1488f816528b35b944f2155d25687c8
Matijs van Zuijlen <matijs@matijs.net>
2026-01-24 11:21:58 +0100

    Do not nest step implementations in testing framework step definitions

--- lib/aruba/cucumber/testing_frameworks.rb
+++ lib/aruba/cucumber/testing_frameworks.rb
@@ -2,39 +2,39 @@
 
 # Cucumber
 Then(/^the feature(?:s)? should not(?: all)? pass$/) do
-  step 'the output should contain " failed)"'
-  step 'the exit status should be 1'
+  expect(all_output).to include_output_string ' failed)'
+  expect(last_command_stopped).to have_exit_status 1
 end
 
 # Cucumber
 Then(/^the feature(?:s)? should(?: all)? pass$/) do
-  step 'the output should not contain " failed)"'
-  step 'the output should not contain " undefined)"'
-  step 'the exit status should be 0'
+  expect(all_output).not_to include_output_string ' failed)'
+  expect(all_output).not_to include_output_string ' undefined)'
+  expect(last_command_stopped).to have_exit_status 0
 end
 
 # Cucumber
 Then(/^the feature(?:s)? should not(?: all)? pass with( regex)?:$/) do |regex, string|
-  step 'the output should contain " failed)"'
-  step 'the exit status should be 1'
+  expect(all_output).to include_output_string ' failed)'
+  expect(last_command_stopped).to have_exit_status 1
 
   if regex
-    step "the output should match %r<#{string}>"
+    expect(all_output).to match_output_string(string)
   else
-    step 'the output should contain:', string
+    expect(all_output).to include_output_string(string)
   end
 end
 
 # Cucumber
 Then(/^the feature(?:s)? should(?: all)? pass with( regex)?:$/) do |regex, string|
-  step 'the output should not contain " failed)"'
-  step 'the output should not contain " undefined)"'
-  step 'the exit status should be 0'
+  expect(all_output).not_to include_output_string ' failed)'
+  expect(all_output).not_to include_output_string ' undefined)'
+  expect(last_command_stopped).to have_exit_status 0
 
   if regex
-    step "the output should match %r<#{string}>"
+    expect(all_output).to match_output_string(string)
   else
-    step 'the output should contain:', string
+    expect(all_output).to include_output_string(string)
   end
 end
 
@@ -42,41 +42,41 @@ end
 Then(/^the spec(?:s)? should not(?: all)? pass(?: with (\d+) failures?)?$/) \
   do |count_failures|
   if count_failures.nil?
-    step 'the output should not contain "0 failures"'
+    expect(all_output).not_to include_output_string '0 failures'
   else
-    step %(the output should contain "#{count_failures} failures")
+    expect(all_output).to include_output_string "#{count_failures} failures"
   end
 
-  step 'the exit status should be 1'
+  expect(last_command_stopped).to have_exit_status 1
 end
 
 # RSpec
 Then(/^the spec(?:s)? should all pass$/) do
-  step 'the output should contain "0 failures"'
-  step 'the exit status should be 0'
+  expect(all_output).to include_output_string '0 failures'
+  expect(last_command_stopped).to have_exit_status 0
 end
 
 # RSpec
 Then(/^the spec(?:s)? should not(?: all)? pass with( regex)?:$/) do |regex, string|
-  step 'the output should not contain "0 failures"'
-  step 'the exit status should be 1'
+  expect(all_output).not_to include_output_string '0 failures'
+  expect(last_command_stopped).to have_exit_status 1
 
   if regex
-    step "the output should match %r<#{string}>"
+    expect(all_output).to match_output_string(string)
   else
-    step 'the output should contain:', string
+    expect(all_output).to include_output_string(string)
   end
 end
 
 # RSpec
 Then(/^the spec(?:s)? should(?: all)? pass with( regex)?:$/) do |regex, string|
-  step 'the output should contain "0 failures"'
-  step 'the exit status should be 0'
+  expect(all_output).to include_output_string '0 failures'
+  expect(last_command_stopped).to have_exit_status 0
 
   if regex
-    step "the output should match %r<#{string}>"
+    expect(all_output).to match_output_string(string)
   else
-    step 'the output should contain:', string
+    expect(all_output).to include_output_string(string)
   end
 end
 
@@ -84,40 +84,40 @@ end
 Then(/^the tests(?:s)? should not(?: all)? pass(?: with (\d+) failures?)?$/) \
   do |count_failures|
   if count_failures.nil?
-    step 'the output should not contain "0 errors"'
+    expect(all_output).not_to include_output_string '0 errors'
   else
-    step %(the output should contain "#{count_failures} errors")
+    expect(all_output).to include_output_string "#{count_failures} errors"
   end
 
-  step 'the exit status should be 1'
+  expect(last_command_stopped).to have_exit_status 1
 end
 
 # Minitest
 Then(/^the tests(?:s)? should all pass$/) do
-  step 'the output should contain "0 errors"'
-  step 'the exit status should be 0'
+  expect(all_output).to include_output_string '0 errors'
+  expect(last_command_stopped).to have_exit_status 0
 end
 
 # Minitest
 Then(/^the test(?:s)? should not(?: all)? pass with( regex)?:$/) do |regex, string|
-  step 'the output should contain "0 errors"'
-  step 'the exit status should be 1'
+  expect(all_output).to include_output_string '0 errors'
+  expect(last_command_stopped).to have_exit_status 1
 
   if regex
-    step "the output should match %r<#{string}>"
+    expect(all_output).to match_output_string(string)
   else
-    step 'the output should contain:', string
+    expect(all_output).to include_output_string(string)
   end
 end
 
 # Minitest
 Then(/^the test(?:s)? should(?: all)? pass with( regex)?:$/) do |regex, string|
-  step 'the output should not contain "0 errors"'
-  step 'the exit status should be 0'
+  expect(all_output).not_to include_output_string '0 errors'
+  expect(last_command_stopped).to have_exit_status 0
 
   if regex
-    step "the output should match %r<#{string}>"
+    expect(all_output).to match_output_string(string)
   else
-    step 'the output should contain:', string
+    expect(all_output).to include_output_string(string)
   end
 end
