8fd5233c60c90d9eee5bcfe03cff8a98674fadd3
Matijs van Zuijlen <matijs@matijs.net>
2026-02-01 14:40:57 +0100

    WIP

--- lib/aruba/cucumber/file.rb
+++ lib/aruba/cucumber/file.rb
@@ -1,7 +1,8 @@
 # frozen_string_literal: true
 
 Given(/^I use (?:a|the) fixture(?: named)? "([^"]*)"$/) do |name|
-  copy File.join(aruba.config.fixtures_path_prefix, name), '.'
+  remove name
+  copy File.join(aruba.config.fixtures_path_prefix, name), name
   cd name
 end
 
