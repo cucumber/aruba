67e3e51a93333b8b1a1899f10f0e1f2ab75cf840
Matijs van Zuijlen <matijs@matijs.net>
2026-07-24 09:10:34 +0200

    Remove deprecated step definitions

--- lib/aruba/cucumber/testing_frameworks.rb
+++ lib/aruba/cucumber/testing_frameworks.rb
@@ -6,10 +6,7 @@
 # This defines the following steps:
 #
 # 1. Simple pass/not pass/fail
-# 2. Pass/not pass/fail with a multiline string or regex
-#
-# For item 2, the 'not pass' variant is deprecated because the wording makes it
-# unclear how the negation should be applied.
+# 2. Pass/fail with a multiline string or regex
 #
 Then(/^the feature(?:s)? should fail$/) do
   expect(all_output).to include_output_string ' failed)'
@@ -38,20 +35,6 @@ Then(/^the feature(?:s)? should fail with( regex)?:$/) do |regex, string|
   end
 end
 
-Then(/^the feature(?:s)? should not(?: all)? pass with( regex)?:$/) do |regex, string|
-  Aruba.platform.deprecated \
-    'This cucumber step has confusing wording and is deprecated.' \
-    "Use 'the feature(s) should fail with#{regex}:' instead."
-  expect(all_output).to include_output_string ' failed)'
-  expect(last_command_stopped).to have_exit_status 1
-
-  if regex
-    expect(all_output).to match_output_string(string)
-  else
-    expect(all_output).to include_output_string(string)
-  end
-end
-
 Then(/^the feature(?:s)? should(?: all)? pass with( regex)?:$/) do |regex, string|
   expect(all_output).not_to include_output_string ' failed)'
   expect(all_output).not_to include_output_string ' undefined)'
@@ -70,11 +53,8 @@ end
 # This defines the following steps:
 #
 # 1. Simple pass/not pass/fail
-# 2. Not pass/fail with a failure count
-# 3. Pass/not pass/fail with a multiline string or regex
-#
-# For items 2 and 3, the 'not pass' variant is deprecated because the wording
-# makes it unclear how the negation should be applied.
+# 2. Fail with a failure count
+# 3. Pass/fail with a multiline string or regex
 #
 Then(/^the spec(?:s)? should fail$/) do
   expect(all_output).not_to include_output_string '0 failures'
@@ -97,30 +77,7 @@ Then(/^the spec(?:s)? should fail with (\d+) failures?$/) do |count|
   expect(last_command_stopped).to have_exit_status 1
 end
 
-Then(/^the spec(?:s)? should not(?: all)? pass with (\d+) failures?$/) do |count|
-  Aruba.platform.deprecated \
-    'This rspec step has confusing wording and is deprecated.' \
-    "Use 'the spec(s) should fail with #{count} failures' instead."
-  expect(all_output).to include_output_string "#{count} failures"
-
-  expect(last_command_stopped).to have_exit_status 1
-end
-
 Then(/^the spec(?:s)? should fail with( regex)?:$/) do |regex, string|
-  expect(all_output).not_to include_output_string '0 failures'
-  expect(last_command_stopped).to have_exit_status 1
-
-  if regex
-    expect(all_output).to match_output_string(string)
-  else
-    expect(all_output).to include_output_string(string)
-  end
-end
-
-Then(/^the spec(?:s)? should not(?: all)? pass with( regex)?:$/) do |regex, string|
-  Aruba.platform.deprecated \
-    'This rspec step has confusing wording and is deprecated.' \
-    "Use 'the spec(s) should fail with#{regex}:' instead."
   expect(all_output).not_to include_output_string '0 failures'
   expect(last_command_stopped).to have_exit_status 1
 
@@ -148,11 +105,8 @@ end
 # This defines the following steps:
 #
 # 1. Simple pass/not pass/fail
-# 2. Not pass/fail with a failure count
-# 3. Pass/not pass/fail with a multiline string or regex
-#
-# For items 2 and 3, the 'not pass' variant is deprecated because the wording
-# makes it unclear how the negation should be applied.
+# 2. Fail with a failure count
+# 3. Pass/fail with a multiline string or regex
 #
 Then(/^the test(?:s)? should fail$/) do
   expect(all_output).not_to include_output_string '0 errors'
@@ -174,29 +128,7 @@ Then(/^the test(?:s)? should fail with (\d+) errors?$/) do |count|
   expect(last_command_stopped).to have_exit_status 1
 end
 
-Then(/^the tests(?:s)? should not(?: all)? pass with (\d+) failures?$/) do |count|
-  Aruba.platform.deprecated \
-    'This minitest step has confusing wording and is deprecated.' \
-    "Use 'the test(s) should fail #{count} errors' instead."
-  expect(all_output).to include_output_string "#{count} errors"
-  expect(last_command_stopped).to have_exit_status 1
-end
-
 Then(/^the test(?:s)? should fail with( regex)?:$/) do |regex, string|
-  expect(all_output).not_to include_output_string '0 errors'
-  expect(last_command_stopped).to have_exit_status 1
-
-  if regex
-    expect(all_output).to match_output_string(string)
-  else
-    expect(all_output).to include_output_string(string)
-  end
-end
-
-Then(/^the test(?:s)? should not(?: all)? pass with( regex)?:$/) do |regex, string|
-  Aruba.platform.deprecated \
-    'This minitest step has confusing wording and is deprecated.' \
-    "Use 'the test(s) should fail with#{regex}:' instead."
   expect(all_output).not_to include_output_string '0 errors'
   expect(last_command_stopped).to have_exit_status 1
 
